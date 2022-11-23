a736c4c5f6ab2482794296029c53dc3f7d8ce6c5
Sannidhya Pathania <pathaniasannidhya@gmail.com>
2022-11-22 17:33:03 -0700

    initial

---
+++ src/Observable.java
@@ -0,0 +1,36 @@
+package src;
+
+import java.util.*;
+
+/**
+ * The Observable is an abstract class. 
+ * It will be observed by the Observer.
+ * It will notify all its observers whenever there's a change in dataset. 
+ */
+abstract class Observable {
+    List<Observer> observers = new ArrayList<>(3);
+
+    /**
+     * This method will add the Observer to the list.
+     * @param observer the Observer object to be added.
+     */
+    public void addObserver(Observer observer){
+        this.observers.add(observer);
+    }
+
+    /**
+     * This method will remove the Observer from the list.
+     * @param observer the Observer object to be removed.
+     */
+    public void removeObserver(Observer observer){
+        this.observers.remove(observer);
+    }
+
+    /**
+     * This method will notify all its Observers about the change in the dataset.
+     */
+    public void notifying(){
+        for( Observer ob : observers)
+            ob.update(this);
+    }
+}
---
+++ src/Observer.java
@@ -0,0 +1,15 @@
+package src;
+
+/**
+ * This is an Interface for implementing Observer classes.
+ */
+interface Observer {    
+
+    /**
+     * The Update method will be notified via Observable whenever there is 
+     * change in the dataset.
+     * @param observable The observable object which can be referenced to get the 
+     * changed dataset.
+     */
+    public void update(Observable observable);
+}
